c4f7692334dc41721fb9e32f0a130eeaf60f8bec
TT <tapan.d.thaker@gmail.com>
2017-09-09 22:33:58 +0530

    Reorganising boilerplate code.

---
+++ FlickrDemo/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  FlickrDemo
+//
+//  Created by Tapan Thaker on 09/09/17.
+//  Copyright © 2017 tapthaker. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
