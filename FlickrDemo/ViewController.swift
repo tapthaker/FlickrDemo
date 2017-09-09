aa27936b8d490a5c95c4f2d083d77a4f2d488a28
TT <tapan.d.thaker@gmail.com>
2017-09-09 22:24:52 +0530

    Initial Commit

---
+++ FlickrDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrDemo
+//
+//  Created by Tapan Thaker on 09/09/17.
+//  Copyright © 2017 tapthaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
